db7acf86f13f4b71b671e5401cfa99fa38bfb11e
Abdulselam94 <abdulselam.taye-ug@aau.edu.et>
2026-04-12 21:17:22 +0300

    feat: refactor search component to improve query handling and debounce search input

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,40 +2,42 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
-
-export default function Search({ placeholder }: { placeholder: string })
-
-{
+export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  const {replace} = useRouter();
+  const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    const param= new URLSearchParams(searchParams);
+  
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(searchParams);
+
     if (term) {
-      param.set('query', term);
+      params.set('query', term);
     } else {
-      param.delete('query');
+      params.delete('query');
     }
-    replace(`${pathname}?${param.toString()}`);
 
-    console.log(term);
-  }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
+
       <input
+        id="search"
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e)=>{
-          handleSearch(e.target.value)
-          
-        }}
         defaultValue={searchParams.get('query')?.toString()}
+        onChange={(e) => handleSearch(e.target.value)}
       />
+
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
