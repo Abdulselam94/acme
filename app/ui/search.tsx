723d78a908337522cf0782ce7ce5af49b296eefd
Abdulselam94 <abdulselam.taye-ug@aau.edu.et>
2026-04-12 20:51:59 +0300

    feat: update search component to handle query parameters and improve search functionality

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,9 +1,25 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
-export default function Search({ placeholder }: { placeholder: string }) {
+
+export default function Search({ placeholder }: { placeholder: string })
+
+{
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const {replace} = useRouter();
+
   function handleSearch(term: string) {
+    const param= new URLSearchParams(searchParams);
+    if (term) {
+      param.set('query', term);
+    } else {
+      param.delete('query');
+    }
+    replace(`${pathname}?${param.toString()}`);
+
     console.log(term);
   }
   return (
@@ -16,8 +32,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
         placeholder={placeholder}
         onChange={(e)=>{
           handleSearch(e.target.value)
-        }
-        }
+          
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
